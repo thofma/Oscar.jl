f1c7792dbc5013548599bb14d83ac9b6a5e40d12
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2023-09-18 10:21:39 +0200

    Tests: add comments for more disabled graded modules tests

--- test/Modules/runtests.jl
+++ test/Modules/runtests.jl
@@ -10,3 +10,10 @@ include("local_rings.jl")
 include("MPolyQuo.jl")
 include("homological-algebra.jl")
 include("ProjectiveModules.jl")
+
+# FIXME: broken, probably obsolete?
+# include("GradedModules.jl")
+
+
+# TODO: the corresponding source file is also commented
+# include("FreeModules-graded.jl")
